e45777818a2cfcf292ae073edf1846ada1b49a85
cantte <cantte098@gmail.com>
2022-01-19 15:09:32 -0500

    feat: add main layout and configure theme

---
+++ components/layouts/main.tsx
@@ -0,0 +1,23 @@
+import { Box, Container } from '@mui/material'
+import Navbar from 'components/Navbar'
+import { ReactNode } from 'react'
+import { NextRouter } from 'next/router'
+
+interface Props {
+  children: ReactNode;
+  router: NextRouter;
+}
+
+const Main = ({ children }: Props) => {
+  return (
+    <Box sx={{ paddingBottom: 8 }}>
+      <Navbar />
+
+      <Container maxWidth="md">
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material'
+import Main from 'components/layouts/main'
+import { AnimatePresence } from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+    secondary: {
+      main: '#fff',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#fff',
+    },
+  },
+})
+
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Main router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Main>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
